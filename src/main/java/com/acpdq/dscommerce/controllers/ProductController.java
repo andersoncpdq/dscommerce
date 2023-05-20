b1909058f32e40532d138fefe5075d44e4d8895f
Anderson Couto <andersoncpdq@gmail.com>
2023-05-20 10:11:45 -0300

    POST new product

--- src/main/java/com/acpdq/dscommerce/controllers/ProductController.java
+++ src/main/java/com/acpdq/dscommerce/controllers/ProductController.java
@@ -1,12 +1,18 @@
 package com.acpdq.dscommerce.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.acpdq.dscommerce.dto.ProductDTO;
 import com.acpdq.dscommerce.services.ProductService;
@@ -19,12 +25,20 @@ public class ProductController {
 	private ProductService service;
 	
 	@GetMapping(value = "/{id}")
-	public ProductDTO findById(@PathVariable Long id) {
-		return service.findById(id);
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+		return ResponseEntity.ok( service.findById(id) );
 	}
 	
 	@GetMapping
-	public Page<ProductDTO> findAll(Pageable pageable) {
-		return service.findAll(pageable);
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+		return ResponseEntity.ok( service.findAll(pageable) );
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				  .path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
